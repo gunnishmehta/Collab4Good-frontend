e5880e849bd2359e39fa46cea51db14b63324317
Jatin Kaushal <92943010+jkaus324@users.noreply.github.com>
2024-02-24 16:26:50 +0530

    NAVBAR + login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
+        <nav className="navbar navbar-expand-lg navbar-light py-3 sticky-top">
             <div className="container">
                 <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">Collab4Good</NavLink>
                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,7 +14,7 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav m-auto my-2 text-center">
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/">Home </NavLink>
+                            <NavLink className="nav-link" to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/events">Events</NavLink>
@@ -33,11 +34,9 @@ const Navbar = () => {
                         <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> Register</NavLink>
                     </div>
                 </div>
-
-
             </div>
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,106 +1,47 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import { server } from '../App'
+import React from 'react';
+import Navbar from '../components/Navbar.jsx';
+import '../styles/Login.css';
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+const LoginSection = () => {
+    return (
+      <>
+      <Navbar/>
+        <section className="vh-100 gradient-custom">
+            <div className="container py-5 h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100 ">
+                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div className="card bg-dark text-white" style={{ borderRadius: "1rem" }}>
+                            <div className="card-body p-5 text-center login-container">
+                                <div className="mb-md-5 mt-md-4 pb-5" >
+                                    <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                                    <p className="text-white-50 mb-5">Please enter your login and password!</p>
+                                    <div className="form-outline form-white mb-4">
+                                        <input type="email" id="typeEmailX" className="form-control form-control-lg" />
+                                        <label className="form-label" htmlFor="typeEmailX">Email</label>
+                                    </div>
+                                    <div className="form-outline form-white mb-4">
+                                        <input type="password" id="typePasswordX" className="form-control form-control-lg" />
+                                        <label className="form-label" htmlFor="typePasswordX">Password</label>
+                                    </div>
+                                    <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
+                                    <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                                    <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                                        <a href="#!" className="text-white"><i className="fab fa-facebook-f fa-lg"></i></a>
+                                        <a href="#!" className="text-white"><i className="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                                        <a href="#!" className="text-white"><i className="fab fa-google fa-lg"></i></a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="mb-0">Don't have an account? <a href="#!" className="text-white-50 fw-bold">Sign Up</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    );
+}
 
-  const [activeForm, setActiveForm] = useState("individual");
-
-  const handleFormChange = (event) => {
-    setActiveForm(event.target.value);
-  };
-
-  const forms = ["individual", "corporate", "ngo"];
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post(`${server}/auth/login`, { email, password });
-      localStorage.setItem("email", email);
-      // Handle successful login response
-      console.log(response.data);
-    } catch (error) {
-      // Handle error
-      setError("Failed to log in. Please check your credentials.");
-      console.error("Login error:", error);
-    }
-  };
-
-  return (
-    <>
-      <Navbar />
-      <div className="container my-3 py-3">
-        <h1 className="text-center">Login</h1>
-        <hr />
-        <div class="row my-4 h-100">
-          <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-            <form onSubmit={handleSubmit}>
-              <div class="my-3">
-                <label for="display-4">Select the type of User</label>
-                <select
-                  value={activeForm}
-                  onChange={handleFormChange}
-                  className="form-control"
-                >
-                  {forms.map((form) => (
-                    <option key={form} value={form}>
-                      {form}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div class="my-3">
-                <label for="display-4">Email address</label>
-                <input
-                  type="email"
-                  class="form-control"
-                  id="floatingInput"
-                  placeholder="name@example.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div class="my-3">
-                <label for="floatingPassword display-4">Password</label>
-                <input
-                  type="password"
-                  class="form-control"
-                  id="floatingPassword"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </div>
-              {error && <p className="text-danger">{error}</p>}
-              <div className="my-3">
-                <p>
-                  New Here?{" "}
-                  <Link to="/register" className="text-decoration-underline text-info">
-                    Register
-                  </Link>{" "}
-                </p>
-              </div>
-              <div className="text-center">
-                <button class="my-2 mx-auto btn btn-dark" type="submit">
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
-};
-
-export default Login;
+export default LoginSection;
