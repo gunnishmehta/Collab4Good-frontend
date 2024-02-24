117eda72a7e6627572222cb6c5edbab119476649
darkmoon-6524 <gunnishmehta2003@gmail.com>
2024-02-25 03:53:02 +0530

    made custom event page

--- src/pages/CustomEvent.jsx
+++ src/pages/CustomEvent.jsx
@@ -2,8 +2,20 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../styles/CustomEvent.css";
+import { Chart } from 'react-google-charts'
 
 const CustomEvent = () => {
+  const data = [
+    ["Company", "Thousands of dollars"],
+    ["A", 11],
+    ["B", 2],
+    ["C", 2],
+    ["D", 2],
+    ["E", 7],
+  ];
+  const options = {
+    is3D: true,
+  };
   const event = {
     name: "Charity Fundraiser",
     date: "February 20, 2024",
@@ -17,13 +29,54 @@ const CustomEvent = () => {
   return (
     <>
       <Navbar />
+
       <div className="eventSection1">
         <h3 className="eventSection1-heading">{event.name}</h3>
       </div>
-      <div className="container">
-        <div className="event-section">
+      <div className="eventSection2 d-flex">
+        <div className="customEventContainer">
+          <div className="customEventDescription">
+            <div className="customEventPicture"></div>
+            <h2>Let's education for children get good life</h2>
+            <p>Lorem ipsum dolor sit amet, cibo mundi ea duo, vim exerci phaedrum. There are many variations of
+              passages of Lorem Ipsum available, but the majority have alteration in some injected or words which
+              don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be
+              sure there isn't anything embarrang hidden in the middle of text. All the Lorem Ipsum generators on the
+              Internet tend to repeat predefined chunks as necessary, making this the first true generator on the
+              Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence
+              structures, to generate Lorem Ipsum which looks reasonable.</p>
+          </div>
+          <div className="customEventDonate">
+            <button type="button" className="btn btn-lg customEventDonate-btn">Donate</button>
+          </div>
+          <div className="summary">
+            <p>Lorem Ipsum has been the Industry's standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type simen book.</p>
+            <ul className="summary-list">
+              <li>Nsectetur cing do not elit.</li>
+              <li>Suspe ndisse suscipit sagittis in Ieo.</li>
+              <li>Entum estibulum dignissim lipsm posuere.</li>
+            </ul>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. orem Ipsum has been the
+              industry's standard dummy text ever since the when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book.</p>
+          </div>
+          <div>
+            <h2 className="mb-4">Donations Received</h2>
+            <div className="row">
+              <Chart
+                chartType="PieChart"
+                data={data}
+                options={options}
+                width={"100%"}
+                height={"400px"}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="customEventCreated">
           <div className="event-info">
-            <h3 className="event-heading">{event.name}</h3>
+
             <div className="event-details" style={{ fontSize: "1.2rem", color: "navy" }}>
               <div className="event-details-row">
                 <strong>Date:</strong> {event.date}
@@ -31,49 +84,32 @@ const CustomEvent = () => {
               <div className="event-details-row">
                 <strong>Organizer:</strong> {event.organizer}
               </div>
-              <div className="donationContainer mt-4">
-                <input
-                  type="range"
-                  name="donation"
-                  id={event.id}
-                  disabled={true}
-                  value={36}
-                  min={1}
-                  max={100}
-                />
-                <div className="d-flex justify-content-between">
-                  <span>
-                    <strong>$36,000</strong> raised
-                  </span>
-                  <span>
-                    <strong>$100,000</strong> raised
-                  </span>
-                </div>
-              </div>
+
+
             </div>
           </div>
           <div className="event-description">
-            <p>{event.description}</p>
-            <p>
-              <strong>Location:</strong> {event.location}
-            </p>
             <div className="top-donors">
-              <h4 className="text-primary">Top Donors:</h4> {/* Increased size and added color */}
+
+              <h4 className="text-primary">Top Donors:</h4>
               <ul>
                 {event.topDonors.map((donor, index) => (
-                  <li key={index} className="text-success"> {/* Highlighting with color */}
+                  <li key={index} className="text-success">
                     {donor}
                   </li>
                 ))}
               </ul>
-              <div className="event-buttons">
-                <button className="btn btn-primary btn-lg mr-2 joinbtn">Join</button>{"                  "}
-                <button className="btn btn-success btn-lg">Donate</button>
-              </div>
             </div>
           </div>
         </div>
+
       </div>
+      {/* 
+      <div className="container">
+        <div className="event-section">
+          
+        </div>
+      </div> */}
       <Footer />
     </>
   );
